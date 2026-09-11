2800481639a57cd72dd0e8b2d77ef5c61e8b5c2b
anYuJia <2259202040@qq.com>
2026-09-11 17:06:35 +0800

    chore: fix release workflow and bump version

--- apps/flutter_client/lib/core/state/settings_store.dart
+++ apps/flutter_client/lib/core/state/settings_store.dart
@@ -66,9 +66,8 @@ class SettingsStore extends ChangeNotifier {
             }
             effectiveToken = legacyToken;
           }
-          var restoredSettings = (await _migrateSettings(
-            loadedSettings,
-          )).copyWith(authToken: effectiveToken);
+          var restoredSettings = (await _migrateSettings(loadedSettings))
+              .copyWith(authToken: effectiveToken);
           final encryptedAdminPassword = loadedSettings
               .macosAdminPasswordCiphertext
               .trim();
@@ -100,9 +99,8 @@ class SettingsStore extends ChangeNotifier {
           }
         }
       } else {
-        _settings = (await _migrateSettings(
-          _settings,
-        )).copyWith(authToken: (await _tokenRepository.read()) ?? '');
+        _settings = (await _migrateSettings(_settings))
+            .copyWith(authToken: (await _tokenRepository.read()) ?? '');
       }
       _lastError = null;
     } catch (error) {
@@ -435,9 +433,8 @@ class SettingsStore extends ChangeNotifier {
     );
     try {
       await temp.writeAsString(
-        const JsonEncoder.withIndent(
-          '  ',
-        ).convert(_persistedSettings().toJson()),
+        const JsonEncoder.withIndent('  ')
+            .convert(_persistedSettings().toJson()),
         flush: true,
       );
       await _restrictFile(temp);
